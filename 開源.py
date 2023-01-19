90ca8b94d327f7d4140c5ef2d01cc31dbb600a2b
yychen1201 <113757435+yychen1201@users.noreply.github.com>
2023-01-19 09:50:40 +0800

    Add files via upload

---
+++ 開源.py
@@ -0,0 +1,37 @@
+import discord,asyncio,aiohttp,json
+from discord import Webhook
+from discord.ext import commands
+from operator import iadd
+from discord.utils import get 
+from discord.commands import Option,slash_command
+
+
+
+bot = commands.Bot(command_prefix="!" , intents=discord.Intents.all()) 
+
+
+#上線回應
+@bot.event
+async def on_ready():
+    print("本開源由 我是人#8315 製作")
+    print("請勿修改")
+    print("有問題請至 https://discord.gg/WDxmw5WjPY 詢問")
+    print("==================================================")
+    print(F"{bot.user}已連線")
+    activity=discord.Activity(type=discord.ActivityType.watching, name="一款好用的webhook發送器") 
+    await bot.change_presence(status=discord.Status.dnd,activity=activity)
+
+
+#webhook功能
+@slash_command()
+@bot.slash_command(name="webhooksend",description="Webhook發送功能")
+async def webhooksend(ctx,url,username,*,msg):
+    async with aiohttp.ClientSession() as session:
+        webhook = Webhook.from_url(F"{url}", session=session)
+        await webhook.send(F"{msg}", username=F"{username}")  
+        await ctx.respond("發送成功",ephemeral=True)
+
+
+
+
+bot.run("TOKEN")
